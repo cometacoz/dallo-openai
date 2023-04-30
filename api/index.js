ca508ac12388de67d18d910ff7be031f18a8675a
Deiby Mejia <dmejia1204@gmail.com>
2023-04-30 02:46:17 -0600

    initialization of web, and add cors setup to api

--- api/index.js
+++ api/index.js
@@ -7,11 +7,11 @@ import { scopePerRequest } from 'awilix-express';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJsdoc from 'swagger-jsdoc';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 
 
 // classes
-import UserController from './controllers/userController.js';
 import ChatController from './controllers/chatController.js';
 import Database from './database/database.js';
 import swaggerOptions from './swagger-jsdoc.js';
@@ -22,7 +22,15 @@ import { firstConversation } from './routes/chat.routes.js';
 
 
 const app = express();
+const cors = require("cors");
+
 const port = process.env.PORT || 3000;
+const corsOptions = {
+    origin: '*',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
+
 
 // Create the container for injected of dependencies;
 const container = createContainer({ injectionMode: InjectionMode.PROXY });
@@ -30,7 +38,6 @@ const container = createContainer({ injectionMode: InjectionMode.PROXY });
 // Register our dependencies in the container;
 container.register({
     database: asClass(Database).singleton(),
-    UserController: asClass(UserController),
     ChatController: asClass(ChatController)
 });
 
@@ -40,13 +47,15 @@ const swaggerSpec = swaggerJsdoc(swaggerOptions);
 // middleware body-parser
 app.use(bodyParser.json());
 
+// middleware cors
+app.use(cors(corsOptions));
+
 
 // Add middleware of Awilix to our application in Express;
 app.use(scopePerRequest(container));
 
 
 // routes
-app.get('/user/:id', (req, res) => getUser(req, res));
 app.post('/conversation/', (req, res) => firstConversation(req, res));
 
 
